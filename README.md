1d36add90264bc6e7af75f4e9e0249625692605d
samyspg7 <samiouali2008@gmail.com>
2021-10-03 22:01:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-qmentn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-qmentn)
---
+++ index.ts
@@ -0,0 +1,10 @@
+let num: number;
+num = parseInt(prompt('ادخل عملية اذا اردت انتعرف ماهي الاشارة'));
+if (num > 0) {
+  console.log(' العدد موجب ');
+}
+if (num < 0) {
+  console.log('العدد سالب');
+}
+
+console.log('شكرا لاستعمل برنامجي');
